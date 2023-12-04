e5183d5ff9e4d329da1ce64bef3bf9019ea90c14
xandervanboom <xander.van.boom@student.howest.be>
2023-12-04 15:32:22 +0100

    Add Dependency injection

--- src/Wex1.Elephant.Logger.WebApi/Program.cs
+++ src/Wex1.Elephant.Logger.WebApi/Program.cs
@@ -1,3 +1,10 @@
+using Wex.Elephant.Logger.Infrastructure.Repositories;
+using Wex1.Elephant.Logger.Core.Interfaces.Repositories;
+using Wex1.Elephant.Logger.Core.Interfaces.Services;
+using Wex1.Elephant.Logger.Core.Interfaces.Services.CrudService;
+using Wex1.Elephant.Logger.WebApi.Services;
+using Wex1.Elephant.Logger.WebApi.Services.CrudServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Dependency injection
+//Other services
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IUriService>(options =>
+{
+    var accessor = options.GetRequiredService<IHttpContextAccessor>();
+    var request = accessor.HttpContext.Request;
+    var uri = string.Concat(request.Scheme, "://", request.Host.ToUriComponent());
+    return new UriService(uri);
+});
+
+//Repositories
+builder.Services.AddTransient<IErrorLogRepository, ErrorLogRepository>();
+
+//Crud Services
+builder.Services.AddTransient<IErrorLogCrudService, ErrorLogsCrudService>();
+
+
 
 var app = builder.Build();
 
