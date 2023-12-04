4ab5fef03bdf74a59fac2b4e6070e6b4d082d850
Yong Pok Low <yongpok95@live.nl>
2023-12-04 10:55:29 +0100

    blit circles

--- User Interface/UI.py
+++ User Interface/UI.py
@@ -19,6 +19,20 @@ view = pygame.Surface([500,300])
 view2 = pygame.Surface([500,300])
 view3 = pygame.Surface([500,300])
 
+# controls
+
+# WASD arrows
+# Work in Progress...
+
+# Arrow in middle
+# Is this for adjusting the speed?
+
+# circle buttons
+circle = pygame.Surface((60,60), pygame.SRCALPHA)
+drawCircle = pygame.draw.circle(circle, white, (30,30), 30)
+
+circle2 = pygame.Surface((60,60), pygame.SRCALPHA)
+drawCircle2 = pygame.draw.circle(circle2, white, (30,30), 30)
 
 # the while loop..
 while not exit: 
@@ -26,9 +40,13 @@ while not exit:
     screen.fill(blue)
 
     # blits the views to the screen. the parameters are the x and y coordinates.
-    screen.blit(view, (50,100)) 
-    screen.blit(view2, (600,100))
-    screen.blit(view3, (50,450))
+    screen.blit(view, (50,50)) 
+    screen.blit(view2, (600,50))
+    screen.blit(view3, (50,400))
+    
+    # blits the circles to screen
+    screen.blit(circle, (1000,500))
+    screen.blit(circle2, (1000,575))
 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
