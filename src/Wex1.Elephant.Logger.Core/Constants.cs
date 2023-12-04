9f2f6a0aea0e20944af09ffd0b15d57c915c82ee
xandervanboom <xander.van.boom@student.howest.be>
2023-12-04 10:28:12 +0100

    Create Constants class

---
+++ src/Wex1.Elephant.Logger.Core/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wex1.Elephant.Logger.Core
+{
+    public class Constants
+    {
+        public const string UserName = "Admin";
+        public const string Password = "hMu4P6L_LAMj8t3";
+        public const string ClusterUrl = "2939d3617acc492aa3b3653ac474fdc0.s2.eu.hivemq.cloud";
+        public const string Port = "8883";
+        public const string WebSocketPort = "8884";
+    }
+}
