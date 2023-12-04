8d2125dfaf56b54ad713cebb3b7542c6e490d521
xandervanboom <xander.van.boom@student.howest.be>
2023-12-04 14:03:32 +0100

    Create logsRepositoryBase

---
+++ src/Wex.Elephant.Logger.Infrastructure/Repositories/LogsRepositoryBase.cs
@@ -0,0 +1,67 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wex1.Elephant.Logger.Core;
+using Wex1.Elephant.Logger.Core.Entities;
+using Wex1.Elephant.Logger.Core.Interfaces.Repositories;
+
+
+namespace Wex.Elephant.Logger.Infrastructure.Repositories
+{
+    public class LogsRepositoryBase<T> : ILogsRepositorybase<T> where T : BaseLog
+    {
+
+        protected readonly MongoClient _client;
+        private string _collectionName;
+        protected IMongoCollection<T> _collection;
+        public LogsRepositoryBase(string collectionName)
+        {
+            _collectionName = collectionName;
+            _client = new MongoClient(Constants.MongoDbConnectionUrl);
+            _collection = _client.GetDatabase("Logger").GetCollection<T>(_collectionName);
+        }
+        public IMongoCollection<T> GetAll()
+        {
+            return _collection;
+        }
+        public async Task<IEnumerable<T>> GetListAsync()
+        {
+            return await _collection.Find(_ => true).ToListAsync();
+        }
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            var filter = Builders<T>.Filter
+                .Eq(T => T.Id, $"{id}");
+
+            return await _collection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _collection.InsertOneAsync(entity);
+        }
+
+        public Task<bool> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetPagedData(IEnumerable<T> data, int pageNumber, int pageSize)
+        {
+            return data
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+        }
+
+
+    }
+}
---
+++ src/Wex1.Elephant.Logger.Core/Interfaces/Services/IUriService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wex1.Elephant.Logger.Core.Filters;
+
+namespace Wex1.Elephant.Logger.Core.Interfaces.Services
+{
+    public interface IUriService
+    {
+        Uri GetPageUri(PaginationFilter filter, string route);
+    }
+}
