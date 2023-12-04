df88a23a7ab8a928876096a5948bc98014d70aec
xandervanboom <xander.van.boom@student.howest.be>
2023-12-04 14:12:28 +0100

    Update ILogsRespitoryBase Change GetAll return type

--- src/Wex1.Elephant.Logger.Core/Interfaces/Repositories/ILogsRepositorybase.cs
+++ src/Wex1.Elephant.Logger.Core/Interfaces/Repositories/ILogsRepositorybase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace Wex1.Elephant.Logger.Core.Interfaces.Repositories
 {
     public interface ILogsRepositorybase<T> where T : BaseLog
     {
-        IQueryable<T> GetAll();
+        IMongoCollection<T> GetAll();
         Task<IEnumerable<T>> GetListAsync();
         Task<IEnumerable<T>> GetPagedData(IEnumerable<T> data, int pageNumber, int pageSize);
         Task<T> GetByIdAsync(Guid id);
