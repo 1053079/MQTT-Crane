5dd940f75e89d6c68f6e8f8a7b187046429572a2
xandervanboom <xander.van.boom@student.howest.be>
2023-12-04 11:22:07 +0100

    Update Const class with mongoDb details

--- src/Wex1.Elephant.Logger.Core/Constants.cs
+++ src/Wex1.Elephant.Logger.Core/Constants.cs
@@ -8,10 +8,15 @@ namespace Wex1.Elephant.Logger.Core
 {
     public class Constants
     {
-        public const string UserName = "Admin";
-        public const string Password = "hMu4P6L_LAMj8t3";
-        public const string ClusterUrl = "2939d3617acc492aa3b3653ac474fdc0.s2.eu.hivemq.cloud";
-        public const string Port = "8883";
-        public const string WebSocketPort = "8884";
+        //Mqtt Details
+        public const string MqttUserName = "Admin";
+        public const string MqttPassword = "hMu4P6L_LAMj8t3";
+        public const string MqttClusterUrl = "2939d3617acc492aa3b3653ac474fdc0.s2.eu.hivemq.cloud";
+        public const string MqttPort = "8883";
+        public const string MqttWebSocketPort = "8884";
+        //MongoDb Details
+        public const string MongoDbUserName = "Admin";
+        public const string MongoDbPassword = "Password";
+        public const string MongoDbConnectionUrl = "mongodb+srv://Admin:Password@wx1-elephants-mongodb.kizgodk.mongodb.net/?retryWrites=true&w=majority";
     }
 }
