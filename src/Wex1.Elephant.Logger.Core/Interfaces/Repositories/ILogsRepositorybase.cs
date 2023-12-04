d17358f722df528dbcb0eb0d9e7bd841148691b9
xandervanboom <xander.van.boom@student.howest.be>
2023-12-04 11:45:54 +0100

    Create ILogsRepositoryBase

---
+++ src/Wex1.Elephant.Logger.Core/Interfaces/Repositories/ILogsRepositorybase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wex1.Elephant.Logger.Core.Entities;
+
+namespace Wex1.Elephant.Logger.Core.Interfaces.Repositories
+{
+    public interface ILogsRepositorybase<T> where T : BaseLog
+    {
+        IQueryable<T> GetAll();
+        Task<IEnumerable<T>> GetListAsync();
+        Task<IEnumerable<T>> GetPagedData(IEnumerable<T> data, int pageNumber, int pageSize);
+        Task<T> GetByIdAsync(Guid id);
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+    }
+}
