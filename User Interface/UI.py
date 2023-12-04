ed1aeb504abb4b44b156918f230c5aaf31d89c53
Yong Pok Low <yongpok95@live.nl>
2023-12-04 10:19:03 +0100

    testing...

--- User Interface/UI.py
+++ User Interface/UI.py
@@ -2,15 +2,32 @@ import pygame
   
 pygame.init() 
   
-# Creates the canvas allows you to change the (Width, Height) in px.
-canvas = pygame.display.set_mode((1000, 1000)) 
+# Creates the screen allows you to change the (Width, Height) in px.
+screen = pygame.display.set_mode((1200, 1000)) 
 
-# This is the title of the canvas you see above the screen.
+# This is the title of the screen you see above the screen.
 pygame.display.set_caption("Elephant Crane") 
 exit = False
-  
+
+# Colours that we can use for visualisation
+white = (255,255,255),
+black = (0,0,0),
+blue = (0,0,255)
+
+# Rectangles.. or actually the views
+rectangle = pygame.Surface([100,200])
+rectangle2 = pygame.Surface([100,200])
+rectangle3 = pygame.Surface([100,200])
+
+
+# the while loop..
 while not exit: 
+    screen.fill(blue)
+    screen.blit(rectangle,(200,200))
+    screen.blit(rectangle2,(400,400))
+    screen.blit(rectangle3,(600,600))
+    
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             exit = True
-    pygame.display.update() 
+    pygame.display.flip() 
