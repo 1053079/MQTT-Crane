5a0eb01ea8c2400c1550721c17fe86b34ce921b5
Yong Pok Low <yongpok95@live.nl>
2023-12-05 13:32:25 +0100

    i am subscribed for inputs #7

--- motor.py
+++ motor.py
@@ -1,17 +1,18 @@
-import paho.mqtt.client as mqttclient
+
+import paho.mqtt.client as mqtt
 import time
 
-def on_connect(client,userdata,flags,rc):
-    if rc==0:
-        print("client is connected")
-        global connected
-        connected = True
+def on_connect(client, userdata, flags, rc, properties=None):
+    if rc == 0:
+        print("Connected to broker")
     else:
-        print("client is not connected")
+        print(f"Connection failed with code {rc}")
 
 def on_message(client, userdata,message):
     print("Message received " + str(message.payload.decode("utf-8")))
     print("Topic " + str(message.topic))
+
+
 
 connected= False
 messageReceived= False
@@ -21,9 +22,14 @@ port = 8883
 username = "Admin"
 password = "hMu4P6L_LAMj8t3"
 
-client = mqttclient.Client("MQTT")
+client = mqtt.Client()
+
+# TLS (required for connection)
+client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+
 client.username_pw_set(username, password)
 client.on_connect = on_connect
+client.on_message = on_message
 client.connect(broker, port)
 client.loop_start()
 client.subscribe("keyboard/inputs")
@@ -33,4 +39,4 @@ while connected!= True:
 while messageReceived!= True: 
     time.sleep(0.2)
 
-client.loop_stop()    
+client.loop_forever()    
