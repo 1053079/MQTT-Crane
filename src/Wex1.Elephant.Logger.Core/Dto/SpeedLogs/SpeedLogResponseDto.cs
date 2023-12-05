671713be4d5996cda976de6ae62bc6a829ee08ba
xandervanboom <xander.van.boom@student.howest.be>
2023-12-05 15:21:04 +0100

    Create SpeedLogDtos #26

---
+++ src/Wex1.Elephant.Logger.Core/Dto/SpeedLogs/SpeedLogResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wex1.Elephant.Logger.Core.Dto.SpeedLogs
+{
+    public class SpeedLogResponseDto
+    {
+        public string Id { get; set; }
+        public DateTime EventTimeStamp { get; set; }
+        public double Speed { get; set; }
+        public string EventType { get; set; }
+        public string component { get; set; }
+        public string Description { get; set; }
+    }
+}
