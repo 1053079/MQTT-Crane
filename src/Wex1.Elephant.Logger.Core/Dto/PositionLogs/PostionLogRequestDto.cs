771fc82b400f1962ab6d809c9f1788edcb2dcc8f
Van Boom Xander <xander.van.boom@student.howest.be>
2023-12-07 14:02:37 +0100

    Create PoitionLog Dtos #27

---
+++ src/Wex1.Elephant.Logger.Core/Dto/PositionLogs/PostionLogRequestDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wex1.Elephant.Logger.Core.Dto.PositionLogs
+{
+    public class PostionLogRequestDto
+    {
+        public string Id { get; set; }
+        [Required]
+        public DateTime EventTimeStamp { get; set; }
+        [Required]
+        public string EventType { get; set; }
+        [Required]
+        public string Component { get; set; }
+        [Required]
+        [MaxLength(255, ErrorMessage = "{0} can't be longer then 255 characters.")]
+        public string Description { get; set; }
+        [Required]
+        public double PositionX { get; set; }
+        [Required]
+        public double PositionY { get; set; }
+        [Required]
+        public double PositionZ { get; set; }
+    }
+}
