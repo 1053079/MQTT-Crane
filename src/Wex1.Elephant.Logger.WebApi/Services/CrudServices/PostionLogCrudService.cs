fb67e611ab89f88a6d5d61cbee8cc45a869c3891
Van Boom Xander <xander.van.boom@student.howest.be>
2023-12-07 14:14:36 +0100

    Create PostionLogCrudService #27

---
+++ src/Wex1.Elephant.Logger.WebApi/Services/CrudServices/PostionLogCrudService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using Wex1.Elephant.Logger.Core.Dto.PositionLogs;
+using Wex1.Elephant.Logger.Core.Filters;
+using Wex1.Elephant.Logger.Core.Interfaces.Repositories;
+using Wex1.Elephant.Logger.Core.Interfaces.Services;
+using Wex1.Elephant.Logger.Core.Interfaces.Services.CrudService;
+using Wex1.Elephant.Logger.WebApi.Helpers;
+
+namespace Wex1.Elephant.Logger.WebApi.Services.CrudServices
+{
+    public class PostionLogCrudService : IPositionLogCrudService
+    {
+        private readonly IPositionLogRepository _positionLogRepository;
+        private readonly IUriService _uriService;
+
+        public PostionLogCrudService(
+            IPositionLogRepository positionLogRepository,
+            IUriService uriService)
+        {
+            _positionLogRepository = positionLogRepository;
+            _uriService = uriService;
+        }
+
+        public async Task<IActionResult> GetAllPaged(PaginationFilter filter, HttpRequest request)
+        {
+            var route = request.Path.Value;
+            var validFilter = new PaginationFilter(filter.PageNumber, filter.PageSize);
+
+            var pagedData = await _positionLogRepository.GetPagedData(validFilter.PageNumber, validFilter.PageSize);
+            var totalRecords = await _positionLogRepository.CountRecords();
+
+            if (totalRecords < 0)
+            {
+                return new NotFoundObjectResult("No position logs were found.");
+            }
+            
+            var pagedResponse = PaginationHelper.CreatePagedReponse(pagedData, validFilter, totalRecords,_uriService, route);
+            return new OkObjectResult(pagedResponse);
+        }
+
+        public Task<IActionResult> GetById(ObjectId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Add(PositionLogRequestDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Update(PositionLogRequestDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Delete(ObjectId id)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
