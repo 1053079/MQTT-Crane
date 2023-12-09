fb459cbab3d660848444bd1120aabd2ab3c78f7d
Kangyou-0987432 <0987432@hr.nl>
2023-12-09 13:35:45 +0100

    Update #9 :: Made the design of the containers and the movement is updated in the views

--- View1-pygame/main.py
+++ View1-pygame/main.py
@@ -5,7 +5,7 @@ import sys
 pygame.init()
 
 # Create a display surface
-width, height = 800, 600
+width, height = 500, 300
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("STS Crane")
 
@@ -19,10 +19,9 @@ pink = (255, 192, 203)
 green = (0, 255, 0)
 grey = (169,169,169)
 purple = (128, 0, 128)
-purple_rect = pygame.Rect(340, 350, 20, 20)
+purple_rect = pygame.Rect(90, 50, 20, 20)
 purple_speed = 1
 rope_height = 110
-
 clock = pygame.time.Clock()
 
 # Main game loop
@@ -39,22 +38,27 @@ while True:
     if keys[pygame.K_s] and rope_height < 180:
         rope_height += 1
 
-    if keys[pygame.K_a] and purple_rect.left > 330:
+    if keys[pygame.K_a] and purple_rect.left > 80:
         purple_rect.x -= purple_speed
-    if keys[pygame.K_d] and purple_rect.right < 530:
+    if keys[pygame.K_d] and purple_rect.right < 280:
         purple_rect.x += purple_speed
 
     screen.fill(black)
 
     # Your drawings (x, y, width, height)
-    pygame.draw.rect(screen, red, [450, 450, 200, 10])  # Shore
-    pygame.draw.rect(screen, red, [455, 445, 10, 110])  # Shore
-    pygame.draw.rect(screen, red, [635, 445, 10, 110])  # Shore
-    pygame.draw.rect(screen, yellow, [490, 330, 10, 120])  # Crane_Leg
-    pygame.draw.rect(screen, yellow, [330, 340, 200, 10])  # Bridge
+    pygame.draw.rect(screen, red, [200, 150, 200, 10])  # Shore
+    pygame.draw.rect(screen, red, [205, 145, 10, 110])  # Shore
+    pygame.draw.rect(screen, red, [385, 145, 10, 110])  # Shore
+    pygame.draw.rect(screen, yellow, [240, 30, 10, 120])  # Crane_Leg
+    pygame.draw.rect(screen, yellow, [80, 40, 200, 10])  # Bridge
     pygame.draw.rect(screen, purple, purple_rect)  # Crane_Cabin
-    pygame.draw.rect(screen, grey, [0, 550, 800, 50])   # waterline
+    pygame.draw.rect(screen, grey, [0, 250, 800, 50])   # waterline
     pygame.draw.line(screen, purple, (purple_rect.centerx, purple_rect.bottom),(purple_rect.centerx, purple_rect.bottom + rope_height), 5)
+    pygame.draw.polygon(screen, orange, [(50, 200), (100, 250), (150, 200)])  # Boat triangle
+    pygame.draw.rect(screen, green, [55, 185, 40, 15])  # Container 1
+    pygame.draw.rect(screen, blue, [105, 185, 40, 15])  # Container 2
+
+
 
 
 # Update the display
