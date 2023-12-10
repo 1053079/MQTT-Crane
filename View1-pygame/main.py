892afed7bbfa241d71fee7a225094d181f9ad268
Kangyou123 <0987432@hr.nl>
2023-12-10 23:32:26 +0100

    Update #9 :: Changed the size of the crane

--- View1-pygame/main.py
+++ View1-pygame/main.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-
 # Initialize pygame
 pygame.init()
 
@@ -15,13 +14,24 @@ blue = (0, 0, 255)
 yellow = (255, 192, 0)
 red = (255, 0, 0)
 orange = (255, 165, 0)
-pink = (255, 192, 203)
-green = (0, 255, 0)
 grey = (169,169,169)
 purple = (128, 0, 128)
-purple_rect = pygame.Rect(90, 50, 20, 20)
-purple_speed = 1
+
+# Set up Rects
+Cabin = pygame.Rect(90, 50, 20, 20)
+cabin_speed = 1
 rope_height = 110
+Shore = pygame.Rect(200, 150, 115, 10)
+Shore_leg1 = pygame.Rect(205, 145, 10, 110)
+Shore_leg2 = pygame.Rect(300, 145, 10, 110)
+Crane_Leg = pygame.Rect(240, 30, 10, 120)
+Bridge = pygame.Rect(80, 40, 200, 10)
+Container_1 = pygame.Rect(85, 185, 40, 15)
+Container_2 = pygame.Rect(135, 185, 40, 15)
+Waterline = pygame.Rect(0, 250, 800, 50)
+
+
+
 clock = pygame.time.Clock()
 
 # Main game loop
@@ -38,27 +48,25 @@ while True:
     if keys[pygame.K_s] and rope_height < 180:
         rope_height += 1
 
-    if keys[pygame.K_a] and purple_rect.left > 80:
-        purple_rect.x -= purple_speed
-    if keys[pygame.K_d] and purple_rect.right < 280:
-        purple_rect.x += purple_speed
+    if keys[pygame.K_a] and Cabin.left > 80:
+        Cabin.x -= cabin_speed
+    if keys[pygame.K_d] and Cabin.right < 280:
+        Cabin.x += cabin_speed
 
     screen.fill(black)
 
     # Your drawings (x, y, width, height)
-    pygame.draw.rect(screen, red, [200, 150, 200, 10])  # Shore
-    pygame.draw.rect(screen, red, [205, 145, 10, 110])  # Shore
-    pygame.draw.rect(screen, red, [385, 145, 10, 110])  # Shore
-    pygame.draw.rect(screen, yellow, [240, 30, 10, 120])  # Crane_Leg
-    pygame.draw.rect(screen, yellow, [80, 40, 200, 10])  # Bridge
-    pygame.draw.rect(screen, purple, purple_rect)  # Crane_Cabin
-    pygame.draw.rect(screen, grey, [0, 250, 800, 50])   # waterline
-    pygame.draw.line(screen, purple, (purple_rect.centerx, purple_rect.bottom),(purple_rect.centerx, purple_rect.bottom + rope_height), 5)
-    pygame.draw.polygon(screen, orange, [(50, 200), (100, 250), (150, 200)])  # Boat triangle
-    pygame.draw.rect(screen, green, [55, 185, 40, 15])  # Container 1
-    pygame.draw.rect(screen, blue, [105, 185, 40, 15])  # Container 2
-
-
+    pygame.draw.rect(screen, red, Shore)
+    pygame.draw.rect(screen, red, Shore_leg1)
+    pygame.draw.rect(screen, red, Shore_leg2)
+    pygame.draw.rect(screen, yellow, Crane_Leg)
+    pygame.draw.rect(screen, yellow, Bridge)
+    pygame.draw.rect(screen, purple, Cabin)
+    pygame.draw.rect(screen, grey, Waterline)
+    pygame.draw.line(screen, purple, (Cabin.centerx, Cabin.bottom), (Cabin.centerx, Cabin.bottom + rope_height), 5) # Rope
+    pygame.draw.polygon(screen, orange, [(80, 200), (130, 250), (180, 200)])  # Boat triangle
+    pygame.draw.rect(screen, blue, Container_1)
+    pygame.draw.rect(screen, blue, Container_2)
 
 
 # Update the display
