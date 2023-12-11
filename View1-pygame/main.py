288e7bb4e856d6119aeef34294ea6eb4591abf1f
Kangyou123 <0987432@hr.nl>
2023-12-11 08:17:52 +0100

    Update #9 :: Move the container if the rope collides with the container

--- View1-pygame/main.py
+++ View1-pygame/main.py
@@ -21,6 +21,7 @@ purple = (128, 0, 128)
 Cabin = pygame.Rect(90, 50, 20, 20)
 cabin_speed = 1
 rope_height = 110
+container_picked_up = False
 Shore = pygame.Rect(200, 150, 115, 10)
 Shore_leg1 = pygame.Rect(205, 145, 10, 110)
 Shore_leg2 = pygame.Rect(300, 145, 10, 110)
@@ -29,6 +30,9 @@ Bridge = pygame.Rect(80, 40, 200, 10)
 Container_1 = pygame.Rect(85, 185, 40, 15)
 Container_2 = pygame.Rect(135, 185, 40, 15)
 Waterline = pygame.Rect(0, 250, 800, 50)
+original_container_1_position = Container_1.topleft
+original_container_2_position = Container_2.topleft
+picked_up_container_position = (0, 0)
 
 
 
@@ -53,6 +57,21 @@ while True:
     if keys[pygame.K_d] and Cabin.right < 280:
         Cabin.x += cabin_speed
 
+    if keys[pygame.K_SPACE]:
+        if not container_picked_up:
+            if Container_1.colliderect((Cabin.centerx, Cabin.bottom + rope_height, 1, 1)):
+                container_picked_up = True
+                picked_up_container_position = Container_1.topleft
+                Container_1.topleft = (width, height)
+            elif Container_2.colliderect((Cabin.centerx, Cabin.bottom + rope_height, 1, 1)):
+                container_picked_up = True
+                picked_up_container_position = Container_2.topleft
+                Container_2.topleft = (width, height)
+        else:
+            container_picked_up = False
+            Container_1.topleft = original_container_1_position
+            Container_2.topleft = original_container_2_position
+
     screen.fill(black)
 
     # Your drawings (x, y, width, height)
@@ -68,8 +87,10 @@ while True:
     pygame.draw.rect(screen, blue, Container_1)
     pygame.draw.rect(screen, blue, Container_2)
 
+    if container_picked_up:
+        pygame.draw.rect(screen, blue, (Cabin.centerx - 20, Cabin.bottom + rope_height - 15, 40, 15))
 
-# Update the display
+    # Update the display
     pygame.display.flip()
 
     clock.tick(60)
