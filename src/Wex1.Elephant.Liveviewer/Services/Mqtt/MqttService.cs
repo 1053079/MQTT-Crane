ca3b30c5ff7f29244fb1460eefefb77e7c4a82a8
xandervanboom <xander.van.boom@student.howest.be>
2023-12-12 13:44:44 +0100

    Create MqttService for liveviewer #46

---
+++ src/Wex1.Elephant.Liveviewer/Services/Mqtt/MqttService.cs
@@ -0,0 +1,25 @@
+﻿using HiveMQtt.Client;
+using HiveMQtt.Client.Events;
+using HiveMQtt.Client.Options;
+
+namespace Wex1.Elephant.Liveviewer.Services.Mqtt
+{
+    public class MqttService : IMqttService
+    {
+        public async Task<HiveMQClient> CreateMqttClient()
+        {
+            var options = new HiveMQClientOptions
+            {
+                UserName = Constants.MqttUserName,
+                Password = Constants.MqttPassword,
+                Port = Constants.MqttPort,
+                Host = Constants.MqttClusterUrl,
+                UseTLS = true,
+            };
+
+            var mqttClient = new HiveMQClient(options);
+            await mqttClient.ConnectAsync().ConfigureAwait(false);
+            return mqttClient;
+        }
+    }
+}
