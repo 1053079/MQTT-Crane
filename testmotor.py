4eddec94a2a860ce553a162025d5349ec4caa887
Yong Pok Low <yongpok95@live.nl>
2023-12-12 12:11:48 +0100

    correct output for motor speed #14 #7 We get payload from output/motorCabin (speed = 1 (slow) speed = 2(normal) , speed = 3(fast) )

--- testmotor.py
+++ testmotor.py
@@ -20,78 +20,79 @@ def on_message(client, userdata,message):
     movement = payload_data.get("movement")
     speed = payload_data.get("speed")
     lock = payload_data.get("lock")
+    
 
     try:  
       if payload_data: 
        if not lock: ## if lock is not on we will perform these actions
-        if speed == '2': ## normal speed
+        if speed == 2 : ## normal speed
          ## Forward and backward are for the Cabin movements
          if movement == "forward":  
-              print("You have pressed " + movement + " at cabinspeed " + speed)
+              print("You have pressed " + movement + " at cabinspeed 2")
          elif movement == "backward":
-              print("You have pressed " + movement + " at cabinspeed " + speed)
+              print("You have pressed " + movement + " at cabinspeed 2")
          ## Left and right are for the Crane movements 
          elif movement == "left":
-              print("You have pressed " + movement + " at cranespeed " + speed)
+              print("You have pressed " + movement + " at cranespeed 2")
          elif movement == "right":
-              print("You have pressed " + movement + " at cranespeed " + speed)
+              print("You have pressed " + movement + " at cranespeed 2")
           ## Up and down are for the Hoist movements
          elif movement == "up":
-             print("You have pressed " + movement + " at hoistspeed " + speed)
+             print("You have pressed " + movement + " at hoistspeed 2")
          elif movement == "down":    
-             print("You have pressed " + movement + " at hoistspeed " + speed)
+             print("You have pressed " + movement + " at hoistspeed 2")
          else:
              print("invalid key detected")      
 
         ## for fast speed
-        elif speed == '3':
+        elif speed == 3:
         ## Forward and backward are for the Cabin movements
          if movement == "forward":  
-             print("You have pressed " + movement + " at cabinspeed " + speed)
+             print("You have pressed " + movement + " at cabinspeed 3")
          elif movement == "backward":
-             print("You have pressed " + movement + " at cabinspeed " + speed)
+             print("You have pressed " + movement + " at cabinspeed 3")
 
          ## Left and right are for the Crane movements 
          elif movement == "left":
-             print("You have pressed " + movement + " at cranespeed " + speed)
+             print("You have pressed " + movement + " at cranespeed 3")
          elif movement == "right":
-             print("You have pressed " + movement + " at cranespeed " + speed)
+             print("You have pressed " + movement + " at cranespeed 3")
              
 
           ## Up and down are for the Hoist movements
          elif movement == "up":
-              print("You have pressed " + movement + " at hoistspeed " + speed)
+              print("You have pressed " + movement + " at hoistspeed 3")
          elif movement == "down":    
-              print("You have pressed " + movement + " at hoistspeed " + speed)
+              print("You have pressed " + movement + " at hoistspeed 3")
          else:
               print("invalid key detected")  
 
         ## for slow speed      
-        elif speed == '1':   
+        elif speed == 1:   
          ## Forward and backward are for the Cabin movements
          if movement == "forward":  
-              print("You have pressed " + movement + " at cabinspeed" + speed)
+              print("You have pressed " + movement + " at cabinspeed 1")
          elif movement == "backward":
-              print("You have pressed " + movement + " at cabinspeed" + speed)
+              print("You have pressed " + movement + " at cabinspeed 1")
 
          ## Left and right are for the Crane movements 
          elif movement == "left":
-             print("You have pressed " + movement + " at cranespeed " + speed)
+             print("You have pressed " + movement + " at cranespeed 1")
          elif movement == "right":
-             print("You have pressed " + movement + " at cranespeed " + speed)
+             print("You have pressed " + movement + " at cranespeed 1")
              
           ## Up and down are for the Hoist movements
          elif movement == "up":
-              print("You have pressed " + movement + " at hoistspeed " + speed) 
+              print("You have pressed " + movement + " at hoistspeed 1") 
          elif movement == "down":    
-              print("You have pressed " + movement + " at hoistspeed " + speed)
+              print("You have pressed " + movement + " at hoistspeed 1")
          else:
              print("invalid key detected")         
         else:
-         print("There was an error trying to get " + movement)
+         print("There was an error trying to get " + movement + " key")
 
-    except:
-     print("There was an error trying to get movement or lock is on")
+    except Exception as e:
+        print("Error:", e)
     
 connected= False
 messageReceived= False
