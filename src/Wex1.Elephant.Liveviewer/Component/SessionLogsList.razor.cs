7bf696b277438aa271eeffa27fbcf348b540742a
xandervanboom <xander.van.boom@student.howest.be>
2023-12-12 13:45:15 +0100

    Create SessionLogsList razor.cs #46

---
+++ src/Wex1.Elephant.Liveviewer/Component/SessionLogsList.razor.cs
@@ -0,0 +1,37 @@
+﻿using HiveMQtt.Client;
+using HiveMQtt.Client.Events;
+using Microsoft.AspNetCore.Components;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Wex1.Elephant.Liveviewer.Model;
+using Wex1.Elephant.Liveviewer.Services.Mqtt;
+
+namespace Wex1.Elephant.Liveviewer.Component
+{
+    public partial class SessionLogsList
+    {
+        [Inject]
+        protected IMqttService _mqttService { get; set; }
+
+        private HiveMQClient _mqttClient;
+
+        private List<Baselog> _logs = new List<Baselog>();
+
+        protected async override Task OnInitializedAsync()
+        {   
+            _mqttClient = await _mqttService.CreateMqttClient();
+
+            _mqttClient.OnMessageReceived += _mqttClient_OnMessageReceived;
+            await _mqttClient.SubscribeAsync("logger/#");
+            await base.OnInitializedAsync();
+        }
+
+        private async void _mqttClient_OnMessageReceived(object? sender, OnMessageReceivedEventArgs e)
+        {
+            var newLog = JsonSerializer.Deserialize<Baselog>(e.PublishMessage.Payload);
+
+            if(newLog is not null)
+                _logs.Add(newLog);
+        }
+    }
+}
