ff7f41575bf3031e6aaa2c76782abed06b0ea759
Yong Pok Low <yongpok95@live.nl>
2023-12-12 11:10:14 +0100

    testmotor gets data from output #7

--- motor.py
+++ motor.py
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 import time
 import json
 
+client = mqtt.Client()
 
 def on_connect(client, userdata, flags, rc, properties=None):
     if rc == 0:
@@ -20,24 +21,28 @@ def on_message(client, userdata,message):
     movement = payload_data.get("movement")
     speed = payload_data.get("speed")
     lock = payload_data.get("lock")
-
+     
     # topics
     topic_2 = "output/motorCabin"
 
     try:  
-     if payload_data: 
+      if payload_data: 
        if not lock: ## if lock is not on we will perform these actions
         if speed == 'normal': ## normal speed
          ## Forward and backward are for the Cabin movements
          if movement == "forward":  
               print("You have pressed " + movement + " at " + speed + " speed")
+            
          elif movement == "backward":
               print("You have pressed " + movement + " at " + speed + " speed")
+              
          ## Left and right are for the Crane movements 
          elif movement == "left":
               print("You have pressed " + movement + " at " + speed + " speed")
+            
          elif movement == "right":
               print("You have pressed " + movement + " at " + speed + " speed")
+             
           ## Up and down are for the Hoist movements
          elif movement == "up":
               print("You have pressed " + movement + " at " + speed + " speed") 
@@ -71,6 +76,7 @@ def on_message(client, userdata,message):
          ## Forward and backward are for the Cabin movements
          if movement == "forward":  
               print("You have pressed " + movement + " at " + speed + " speed")
+              
          elif movement == "backward":
               print("You have pressed " + movement + " at " + speed + " speed")
          ## Left and right are for the Crane movements 
@@ -91,7 +97,7 @@ def on_message(client, userdata,message):
         # Payload
         payload = {"movement": movement, "speed": speed, "lock": lock}
         payload_string = json.dumps(payload)
-        client.publish(topic_2, payload_string, qos=1)
+        client.publish(topic_2, payload_string, qos=0)
         print(payload) 
     except:
      print("There was an error trying to get movement or lock is on")
@@ -106,7 +112,7 @@ port = 8883
 username = "Admin"
 password = "hMu4P6L_LAMj8t3"
 
-client = mqtt.Client()
+
 
 # TLS (required for connection)
 client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
@@ -118,8 +124,8 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect(broker, port)
+client.subscribe("inputs/joystick") ## have to be subscribed first then client loop start!
 client.loop_start()
-client.subscribe("inputs/joystick")
 
 while connected!= True:
     time.sleep(0.2)
