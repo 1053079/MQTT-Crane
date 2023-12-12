14e5b06b55a3980323d3e86a536ac5e623c59d75
MathisVanhauwe <mathis.vanhauwe@student.howest.be>
2023-12-12 13:40:51 +0100

    Add core project Spreader #17

---
+++ src/Wex1.Elephant.Spreader.Core/Entities/Spreaders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wex1.Elephant.Spreader.Core.Entities
+{
+    public class Spreaders
+    {
+        public double PositionX { get; set; }
+        public double PositionY { get; set; }  
+        public double PositionZ { get; set ;}
+
+        public Sensor Sensor { get; set; }
+        public bool Lock { get; set; }
+
+        
+    }
+}
---
+++ src/Wex1.Elephant.Spreader.Core/Interfaces/Mqtt/IMqttService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HiveMQtt.Client;
+using HiveMQtt.Client.Events;
+using HiveMQtt.Client.Options;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace Wex1.Elephant.Spreader.Core.Interfaces.Mqtt
+{
+    public interface IMqttService
+    {
+
+        Task CreateMqttClient(HiveMQClientOptions options);
+        
+        Task SubscribePositionSpreader();
+
+        Task PublishSensorStatus(bool detectedContainer);
+        Task PublishLockStatus(bool locked);
+        public void _mqttClient_OnMessageReceived(object? sender, OnMessageReceivedEventArgs e);
+        Task HandleMessageAsync(OnMessageReceivedEventArgs e);
+    }
+}
