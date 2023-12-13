8b5529ffc581a6350bd76f902af28c86b4283c7b
xandervanboom <xander.van.boom@student.howest.be>
2023-12-13 12:55:11 +0100

    Update ApiLogProvider to return page instead of list of logs #44

--- src/Wex1.Elephant.Liveviewer/Services/Interfaces/IApiLogProvider.cs
+++ src/Wex1.Elephant.Liveviewer/Services/Interfaces/IApiLogProvider.cs
@@ -1,11 +1,12 @@
 ﻿using MongoDB.Bson;
+using Wex1.Elephant.Liveviewer.Dto;
 using Wex1.Elephant.Liveviewer.Model;
 
 namespace Wex1.Elephant.Liveviewer.Services.Interfaces
 {
-    public interface IApiLogProvider<T> where T : Baselog
+    public interface IApiLogProvider<T, TDto> where T : Baselog where TDto : class
     {
-        Task<IEnumerable<T>> GetAll(int pageNumber, int pageSize);
+        Task<PageDto<TDto>> GetPage(int pageNumber, int pageSize);
         Task<T> Get(string id);
 
     }
