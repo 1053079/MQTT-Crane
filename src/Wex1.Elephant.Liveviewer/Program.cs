4d0261305038057bd64c87a0920045d3c802fabc
xandervanboom <xander.van.boom@student.howest.be>
2023-12-13 12:55:54 +0100

    Add blazor bootstrap #44

--- src/Wex1.Elephant.Liveviewer/Program.cs
+++ src/Wex1.Elephant.Liveviewer/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
+builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddTransient<HttpClient>();
 //builder.Services.AddScoped<ILogProvider<ErrorLog>, LogProvider<ErrorLog>>();
