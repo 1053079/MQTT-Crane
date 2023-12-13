17b5b0919cfc1b870904fad27c403f8abcb239b9
xandervanboom <xander.van.boom@student.howest.be>
2023-12-13 12:56:20 +0100

    Create logmapper #44

---
+++ src/Wex1.Elephant.Liveviewer/Services/Mapper/LogMapper.cs
@@ -0,0 +1,28 @@
+﻿using Wex1.Elephant.Liveviewer.Dto;
+using Wex1.Elephant.Liveviewer.Model;
+
+namespace Wex1.Elephant.Liveviewer.Services.Mapper
+{
+    public static class LogMapper
+    {
+        public static ErrorLog MapToLog(this ErrorDto dto)
+        {
+            return new ErrorLog
+            {
+                Id = dto.Id,
+                Timestamp = dto.Timestamp,
+                Component = dto.Component,
+                Description = dto.Description,
+                Type = dto.Type
+            };
+        }
+
+        public static IEnumerable<ErrorLog> MapToLog(this IEnumerable<ErrorDto> dtos)
+        {
+            return dtos.Select(dto => dto.MapToLog());
+        }
+
+       
+
+    }
+}
