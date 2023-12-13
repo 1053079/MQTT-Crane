f4fe5c5e04cce98ff16861e713edf14e5b5694c0
xandervanboom <xander.van.boom@student.howest.be>
2023-12-13 15:28:21 +0100

    Create DateFilter #75

---
+++ src/Wex1.Elephant.Logger.Core/Filters/DateFilter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wex1.Elephant.Logger.Core.Filters
+{
+    public class DateFilter
+    {
+        public DateOnly? SelectedDate { get; set;}
+        public bool NewestFirst { get; set;}
+
+        public DateFilter()
+        {
+            SelectedDate = null;
+            NewestFirst = true;
+        }
+
+        public DateFilter(DateOnly selectedDate)
+        {
+            var dateNow = DateOnly.FromDateTime(DateTime.UtcNow);
+            SelectedDate = selectedDate > dateNow ? dateNow : selectedDate;
+        }
+
+        public DateFilter(bool newestFirst)
+        {
+            NewestFirst = newestFirst;
+        }
+
+        public DateFilter(DateOnly selectedDate, bool newestFirst)
+        {
+            var dateNow = DateOnly.FromDateTime(DateTime.UtcNow);
+            SelectedDate = selectedDate > dateNow ? dateNow : selectedDate;
+            NewestFirst = newestFirst;
+        }
+
+    }
+}
