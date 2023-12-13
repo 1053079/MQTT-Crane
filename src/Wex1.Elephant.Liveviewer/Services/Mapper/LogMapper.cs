0a7743c2f46a43913cd3f271571288bf3a5998e6
xandervanboom <xander.van.boom@student.howest.be>
2023-12-13 14:25:21 +0100

    Update logmapper #44

--- src/Wex1.Elephant.Liveviewer/Services/Mapper/LogMapper.cs
+++ src/Wex1.Elephant.Liveviewer/Services/Mapper/LogMapper.cs
@@ -21,8 +21,61 @@ namespace Wex1.Elephant.Liveviewer.Services.Mapper
         {
             return dtos.Select(dto => dto.MapToLog());
         }
+        public static ActionLog MapToLOg(this ActionDto dto)
+        {
+            return new ActionLog
+            {
+                Id = dto.Id,
+                Timestamp = dto.Timestamp,
+                Component = dto.Component,
+                Description = dto.Description,
+                Type = dto.Type
+            };
+        }
 
-       
+        public static IEnumerable<ActionLog> MapToLog(this IEnumerable<ActionDto> dtos)
+        {
+            return dtos.Select(dto => dto.MapToLOg());
+        }
+
+        public static PositionLog MapToLog(this PositionDto dto)
+        {
+            return new PositionLog
+            {
+                Id = dto.Id,
+                Timestamp = dto.Timestamp,
+                Component = dto.Component,
+                Description = dto.Description,
+                PositionX = dto.PositionX,
+                PositionY = dto.PositionY,
+                PositionZ = dto.PositionZ,
+                Type = dto.Type
+            };
+        }
+
+        public static IEnumerable<PositionLog> MapToLog(this IEnumerable<PositionDto> dtos)
+        {
+            return dtos.Select(dto => dto.MapToLog());
+        }
+
+        public static SpeedLog MapToLog(this SpeedDto dto)
+        {
+            return new SpeedLog
+            {
+                Id = dto.Id,
+                Timestamp = dto.Timestamp,
+                Component = dto.Component,
+                Description = dto.Description,
+                Speed = dto.Speed,
+                Type = dto.Type
+            };
+        }
+
+        public static IEnumerable<SpeedLog> MapToLog(this IEnumerable<SpeedDto> dtos)
+        {
+            return dtos.Select(dto => dto.MapToLog());
+        }
+
 
     }
 }
