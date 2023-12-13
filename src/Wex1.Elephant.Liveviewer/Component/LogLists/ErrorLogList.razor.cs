79784cb68ece04d396703d762d4cc0764e7274ce
xandervanboom <xander.van.boom@student.howest.be>
2023-12-13 12:55:26 +0100

    Update ErrorLogList add pagination #44

---
+++ src/Wex1.Elephant.Liveviewer/Component/LogLists/ErrorLogList.razor.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using Wex1.Elephant.Liveviewer.Model;
+using Wex1.Elephant.Liveviewer.Services.Interfaces;
+using Wex1.Elephant.Liveviewer.Services.Mapper;
+
+namespace Wex1.Elephant.Liveviewer.Component.LogLists
+{
+    public partial class ErrorLogList
+    {
+        private ErrorLog[] ErrorLogs;
+
+        private string ErrorMessage;
+        private bool IsError;
+
+        private int currentPageNumber = 1;
+        private int pageSize;
+        private int totalPages;
+
+        [Inject]
+        private IApiErrorLogProvider _errorLogProvider { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await FetchErrorLogs();
+        }
+
+        private async Task HandlePageChangeAsync(int newPageNumber)
+        {
+            await Task.Run(() => { currentPageNumber = newPageNumber; }) ;
+            await FetchErrorLogs();
+        }
+
+        private async Task FetchErrorLogs()
+        {
+            try
+            {
+                IsError = false;
+                try
+                {
+                    var pageDto = await _errorLogProvider.GetPage(currentPageNumber, pageSize);
+                    totalPages = pageDto.TotalPages;
+                    ErrorLogs = pageDto.Data.MapToLog().ToArray();
+                    await InvokeAsync(StateHasChanged);
+                } catch(Exception ex)
+                {
+                    IsError = true;
+                    ErrorMessage = ex.Message;
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                IsError = true;
+                ErrorMessage = $"Errorlogs could not be shown due to an error: \n {ex.Message}";
+
+            }
+
+        }
+    }
+}
