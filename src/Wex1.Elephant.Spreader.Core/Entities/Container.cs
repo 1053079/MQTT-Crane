3be7eda8e57ccc79467500e51b629b678a45e6dd
MathisVanhauwe <mathis.vanhauwe@student.howest.be>
2023-12-13 11:46:36 +0100

    Add container entity #17

---
+++ src/Wex1.Elephant.Spreader.Core/Entities/Container.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wex1.Elephant.Spreader.Core.Entities
+{
+    public class Container
+    {
+        public double PositionX { get; set; }
+
+        public double PositionY { get; set; }
+        public double PositionZ { get; set; }
+    }
+}
