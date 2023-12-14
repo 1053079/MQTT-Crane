a64565d3e2b38f268dab6f4c5958781f7981981e
Kangyou123 <0987432@hr.nl>
2023-12-14 14:40:21 +0100

    Add view2 into the UI visualization that moves correctly with the view1 movement

---
+++ All_views/view2.py
@@ -0,0 +1,49 @@
+# view2.py
+
+import pygame
+
+movement_speed = 1
+clock = pygame.time.Clock()
+
+# Colours that we can use for visualisation
+white = (255,255,255),
+black = (0,0,0),
+blue = (0,0,255)
+red = (255,0,0)
+yellow = (255,192,0)
+
+# Set up Rects
+
+shore_x = 795
+shore_y = 200
+Waterline_view2 = pygame.Rect(625, 300, 500, 50)
+Shore_view2 = pygame.Rect(shore_x, shore_y, 110, 10)
+Shore_leg1_view2 = pygame.Rect(shore_x + 10, shore_y - 10, 10, 110)
+Shore_leg2_view2 = pygame.Rect(shore_x + 90, shore_y - 10, 10, 110)
+Crane_Leg_view2 = pygame.Rect(shore_x + 35, shore_y - 120, 10, 120)
+
+def draw_view2(screen,shore_x, shore_y,resized_boat):
+    pygame.draw.rect(screen, yellow, Shore_view2)
+    # blits the boat to screen
+    screen.blit(resized_boat, (777, 255))
+    pygame.draw.rect(screen, blue, Waterline_view2)
+    pygame.draw.rect(screen, yellow, Shore_leg1_view2)
+    pygame.draw.rect(screen, yellow, Shore_leg2_view2)
+    pygame.draw.rect(screen, yellow, Crane_Leg_view2)
+
+    keys = pygame.key.get_pressed()
+
+    # Move Shore_view2 left and right within limits
+    if keys[pygame.K_w] and shore_x > 710:
+        shore_x -= movement_speed
+    if keys[pygame.K_s] and shore_x < 890:
+        shore_x += movement_speed
+
+    Shore_view2.x = shore_x
+    Shore_leg1_view2.x = shore_x + 10
+    Shore_leg2_view2.x = shore_x + 90
+    Crane_Leg_view2.x = shore_x + 50
+
+    clock.tick(30)
+
+    return shore_x, shore_y, resized_boat, Shore_view2, Shore_leg1_view2, Shore_leg2_view2, Crane_Leg_view2, Waterline_view2
--- UI.py
+++ UI.py
@@ -1,8 +1,7 @@
-import pygame 
+import pygame
 import os
 from All_views.view1 import draw_view1
-
-
+from All_views.view2 import draw_view2
 
 # Initialize pygame
 pygame.init()
@@ -10,23 +9,23 @@ clock = pygame.time.Clock()
 # Creates the screen allows you to change the (Width, Height) in px.
 WIDTH = 1200
 HEIGHT = 800
-screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # This is the title of the screen you see above the screen.
-pygame.display.set_caption("Elephant Crane") 
+pygame.display.set_caption("Elephant Crane")
 exit = False
 
 # Colours that we can use for visualisation
-white = (255,255,255)
-black = (0,0,0),
-blue = (0,0,255)
-skyBlue = (0,255,255)
-blueDeFrance = (49,140,231)
+white = (255, 255, 255)
+black = (0, 0, 0),
+blue = (0, 0, 255)
+skyBlue = (0, 255, 255)
+blueDeFrance = (49, 140, 231)
 
 # The screens.. parameters are [Width, Height]
-view = pygame.Surface([500,300])
-view2 = pygame.Surface([500,300])
-view3 = pygame.Surface([500,300])
+view = pygame.Surface([500, 300])
+view2 = pygame.Surface([500, 300])
+view3 = pygame.Surface([500, 300])
 
 # Controls
 
@@ -50,23 +49,37 @@ circle2 = pygame.image.load('UserInterface/buttons/plusButton.png')
 rope_height = 110
 container_picked_up = False
 Container_1 = pygame.Rect(230, 245, 40, 15)
+shore_x = 805
+shore_y = 200
+# boat_view2
+boat_view2 = pygame.image.load('UserInterface/boat_view2.png')
+new_size_view2 = (175, 50)
+resized_boat_view2 = pygame.transform.scale(boat_view2, new_size_view2)
 
 boat_view1 = pygame.image.load('UserInterface/boat_view1.png')
 new_size_view1 = (100, 50)
 resized_boat_view1 = pygame.transform.scale(boat_view1, new_size_view1)
+
+cabin_x = 225
+cabin_y = 545
+legs_y = 500
+crain_y = 550
+legs_bridge1_y = 505
+legs_bridge2_y = 595
+
 font = pygame.font.Font(None, 15)
-text_color = white = (255, 255, 255)
+text_color = (255, 255, 255)
 
 # the while loop..
-while not exit: 
+while not exit:
     # Fills the background with blue color
     screen.fill(blueDeFrance)
 
     # blits the views to the screen. the parameters are the x and y coordinates.
-    screen.blit(view, (75,50))
-    screen.blit(view2, (625,50))
-    screen.blit(view3, (75,400))
-    
+    screen.blit(view, (75, 50))
+    screen.blit(view2, (625, 50))
+    screen.blit(view3, (75, 400))
+
     # Blits WASD Keys
     screen.blit(WASD, (650, 500))
 
@@ -74,17 +87,18 @@ while not exit:
     screen.blit(arrow, (850, 510))
 
     # blits the circles to screen
-    screen.blit(circle, (1025,505))
-    screen.blit(circle2, (1025,585))
+    screen.blit(circle, (1025, 505))
+    screen.blit(circle2, (1025, 585))
 
     screen.blit(resized_boat_view1, (200, 255))
 
     rope_height = draw_view1(screen, rope_height, font, text_color)
-
+    draw_view2(screen, shore_x, shore_y, resized_boat_view2, )
+    shore_x, shore_y, resized_boat_view2, _, _, _, _, _ = draw_view2(screen, shore_x, shore_y, resized_boat_view2)
 
     # if user presses quit the application closes.
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             exit = True
 
     # updates the display
