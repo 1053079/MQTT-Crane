ae0cfc9e4f1d051bdf6af8c2653a36b428d78b54
xandervanboom <xander.van.boom@student.howest.be>
2023-12-14 08:53:12 +0100

    Update ICrudService to use DateFilter #75

--- src/Wex1.Elephant.Logger.Core/Interfaces/Services/CrudService/ICrudService.cs
+++ src/Wex1.Elephant.Logger.Core/Interfaces/Services/CrudService/ICrudService.cs
@@ -8,7 +8,7 @@ namespace Wex1.Elephant.Logger.Core.Interfaces.Services.CrudService
     public interface ICrudService<TDto> where TDto : class
     {
         Task<IActionResult> GetById(ObjectId id);
-        Task<IActionResult> GetAllPaged(PaginationFilter filter, HttpRequest request);
+        Task<IActionResult> GetAllPaged(PaginationFilter paginationFilter, DateFilter dateFilter, HttpRequest request);
         Task<IActionResult> Add(TDto dto);
         Task<IActionResult> Update(TDto dto);
         Task<IActionResult> Delete(ObjectId id);
