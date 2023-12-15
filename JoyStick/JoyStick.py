4d8d3065d6c74a519c8e97416d32a1568a4d17b5
Yong Pok Low <yongpok95@live.nl>
2023-12-15 13:01:11 +0100

    actual adjust joystick #60 its wonky

--- JoyStick/JoyStick.py
+++ JoyStick/JoyStick.py
@@ -25,8 +25,6 @@ client.on_connect = on_connect
 # Pull Request comment 1: TLS (required for connection) - Dit was ook blijkbaar nodig voor de connectie, ook uren aan verspild. TLS zorgt voor een beveiligde connectie.
 # client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS) # Deze versie werkt niet voor Xander, dus we gebruiken de onderstaande versie.
 client.tls_set(cert_reqs=mqtt.ssl.CERT_NONE)
-# client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS) # Deze versie werkt niet voor Xander, dus we gebruiken de onderstaande versie.
-client.tls_set(cert_reqs=mqtt.ssl.CERT_NONE)
 
 # Username and password (required for connection)
 client.username_pw_set("Admin", "hMu4P6L_LAMj8t3")
@@ -64,26 +62,6 @@ try:
                     print('S and D key is pressed')
                     movement = 'backwardRight'
                     break
-                
-                # Individual keys
-                # Combo keys
-                if keyboard.is_pressed('w') and keyboard.is_pressed('a'):
-                    print('W and A key is pressed')
-                    movement = 'forwardLeft'
-                    break
-                if keyboard.is_pressed('w') and keyboard.is_pressed('d'):
-                    print('W and D key is pressed')
-                    movement = 'forwardRight'
-                    break
-                if keyboard.is_pressed('s') and keyboard.is_pressed('a'):
-                    print('S and A key is pressed')
-                    movement = 'backwardLeft'
-                    break
-                if keyboard.is_pressed('s') and keyboard.is_pressed('d'):
-                    print('S and D key is pressed')
-                    movement = 'backwardRight'
-                    break
-                
                 # Individual keys
                 if keyboard.is_pressed('w'):
                     print('W key is pressed')
@@ -135,7 +113,6 @@ try:
 
         # Payload
         payload = {"movement": movement, "speed": speed, "lock": lock, "emergency": emergency} 
-        payload = {"movement": movement, "speed": speed, "lock": lock}
         payload_string = json.dumps(payload)
         client.publish(topic_1, payload_string, qos=0)
         print(payload)
