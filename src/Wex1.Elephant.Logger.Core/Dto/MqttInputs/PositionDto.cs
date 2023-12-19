3b1d528cb2e42f16fd518ee817fafc70739f2e72
xandervanboom <xander.van.boom@student.howest.be>
2023-12-19 09:48:16 +0100

    fix page bug #101

--- src/Wex1.Elephant.Logger.Core/Dto/MqttInputs/PositionDto.cs
+++ src/Wex1.Elephant.Logger.Core/Dto/MqttInputs/PositionDto.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Wex1.Elephant.Logger.Core.Dto.MqttInputs
 {
     public class PositionDto
     {
+        [JsonPropertyName("positionX")]
         public double X { get; set; }
+        [JsonPropertyName("positionY")]
         public double Y { get; set; }
     }
 }
